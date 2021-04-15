6fa357fd3f13115c5817947cb7426742bbcfa6a3
rudberga <rudberg@pm.me>
2021-04-15 12:31:23 +0000

    ADD - field to model in pieces as well as rename them to fit better

--- pieces/admin.py
+++ pieces/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Pieces, Category
+from .models import Piece, Category
 
 # Register your models here.
-admin.site.register(Pieces)
+admin.site.register(Piece)
 admin.site.register(Category)
--- pieces/models.py
+++ pieces/models.py
@@ -13,14 +13,15 @@ class Category(models.Model):
         return self.friendly_name
 
 
-class Pieces(models.Model):
+class Piece(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
-    creator = models.CharField(max_length=254)
+    artist = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    eth_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     year = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    path = models.URLField(max_length=1024, null=True, blank=True)
-    cid = models.ImageField(null=True, blank=True)
+    media_url = models.URLField(max_length=1024, null=True, blank=True)
+    media = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
