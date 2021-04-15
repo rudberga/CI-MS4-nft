ff3ae54208aa7da7d701f8fd70d5f8b012ee0c16
rudberga <rudberg@pm.me>
2021-04-15 13:33:55 +0000

    ADD - urls for pieces page

--- pieces/views.py
+++ pieces/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Piece
 
-# Create your views here.
+
+def all_pieces(request):
+    """ A view to show all pieces, including sorting and search queries """
+
+    pieces = Piece.objects.all()
+
+    context = {
+        'pieces': pieces,
+    }
+
+    return render(request, 'pieces/pieces.html', context)
