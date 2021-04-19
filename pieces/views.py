3225cd19ea1cc9ef8761bb514d2520291cfd507a
rudberga <rudberg@pm.me>
2021-04-19 13:21:26 +0000

    ADD - filtering through categories on pieces page

--- pieces/views.py
+++ pieces/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Piece
+from .models import Category, Piece
 
 
 def all_pieces(request):
@@ -9,12 +9,18 @@ def all_pieces(request):
 
     pieces = Piece.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            pieces = pieces.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(request, "You forgot to enter any search criteria!")
                 return redirect(reverse('pieces'))
            
             queries = Q(name__icontains=query) | Q(type_of_piece__icontains=query)
@@ -23,6 +29,7 @@ def all_pieces(request):
     context = {
         'pieces': pieces,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'pieces/pieces.html', context)
