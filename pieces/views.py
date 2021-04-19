d600a8382118560056cb01aa8a0d7303170148fd
rudberga <rudberg@pm.me>
2021-04-19 12:57:46 +0000

    ADD - search queries

--- pieces/views.py
+++ pieces/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Piece
 
 
@@ -6,9 +8,21 @@ def all_pieces(request):
     """ A view to show all pieces, including sorting and search queries """
 
     pieces = Piece.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('pieces'))
+           
+            queries = Q(name__icontains=query) | Q(type_of_piece__icontains=query)
+            pieces = pieces.filter(queries)
 
     context = {
         'pieces': pieces,
+        'search_term': query,
     }
 
     return render(request, 'pieces/pieces.html', context)
