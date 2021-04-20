1d2d91a573098b2f0ae2c0f1127718b17ab23b24
rudberga <rudberg@pm.me>
2021-04-20 12:07:32 +0000

    ADD - functionality for shopping cart

---
+++ cart/contexts.py
@@ -0,0 +1,20 @@
+from decimal import Decimal
+from django.conf import settings
+
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    product_count = 0
+
+    grand_total = total
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
+
+    return context
--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_cart, name='cart'),
+    path('', views.view_cart, name='view_cart'),
 ]
