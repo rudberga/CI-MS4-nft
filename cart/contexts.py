acbc3b3569a2129d4f4a1c6edfd81b369a50d3c9
rudberga <rudberg@pm.me>
2021-04-20 12:44:02 +0000

    ADD - shopping cart showing pieces that are added

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,19 +1,30 @@
-from decimal import Decimal
-from django.conf import settings
+from django.shortcuts import get_object_or_404
+from pieces.models import Piece
 
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    piece_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        piece = get_object_or_404(Piece, pk=item_id)
+        total += quantity * piece.price
+        piece_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'piece': piece,
+        })
 
     grand_total = total
 
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'piece_count': piece_count,
         'grand_total': grand_total,
     }
 
