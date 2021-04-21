1c0bcbbbb708f4f73a70d766ebe81ce63451942d
rudberga <rudberg@pm.me>
2021-04-21 14:22:20 +0000

    ADD - remove functionality for cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
+from django.contrib import messages
+
+from pieces.models import Piece
 
 
 def view_cart(request):
@@ -21,3 +26,41 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the piece in cart"""
+
+    piece = get_object_or_404(Piece, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+        messages.success(request,
+                         (f'Updated {piece.name} '
+                          f'quantity to {cart[item_id]}'))
+    else:
+        cart.pop(item_id)
+        messages.success(request,
+                         (f'Removed {piece.name} '
+                          f'from your bag'))
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        cart = request.session.get('cart', {})
+        piece = Piece.objects.get(pk=item_id)
+        cart.pop(item_id)
+        messages.success(request, f'Removed {piece.name} from cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
