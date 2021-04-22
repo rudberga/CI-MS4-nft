bb8124bf61a0aeff3150ce5346285afa1b8035ca
rudberga <rudberg@pm.me>
2021-04-22 14:25:34 +0000

    ADD - bag _tool

--- nft/settings.py
+++ nft/settings.py
@@ -80,11 +80,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
             'builtins': [
-                'crispy_form.templatetags.crispy_forms_tags',
-                'crispy_form.templatetags.crispy_forms_field',
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ]
         },
     },
