ad0bc4b2de7a3a0ec0aa1306a59795bf9580ffaf
rudberga <rudberg@pm.me>
2021-04-25 13:21:49 +0000

    ADD - profile url, views and model

--- nft/urls.py
+++ nft/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('pieces/', include('pieces.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
