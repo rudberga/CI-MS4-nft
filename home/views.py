abcb0a586df8d7f0b2cae065f4a04964308f5033
rudberga <rudberg@pm.me>
2021-04-29 13:28:04 +0000

    ADD - about page template, views and urls

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def index(request):
     """ View to return index page """
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ View to go to about page """
+
+    return render(request, 'home/about.html')
