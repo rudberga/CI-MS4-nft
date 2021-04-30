5a0c912faf7ee53f738a4ac60c59f6226f02f7c6
rudberga <rudberg@pm.me>
2021-04-30 21:41:22 +0000

    ADD - removed secret key and set debug

--- nft/settings.py
+++ nft/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0ad341b8oa4%nz*hu5_opdnuiosp+_l)6g)def#+lb(owk889^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['rudberga-nft.herokuapp.com', 'localhost']
 
