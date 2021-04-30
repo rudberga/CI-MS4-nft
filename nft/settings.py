c278499b151dd77049e1bb7818275738fd8ad328
rudberga <rudberg@pm.me>
2021-04-30 21:21:43 +0000

    ADD - database to heroku postgres

--- nft/settings.py
+++ nft/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -134,7 +135,7 @@ WSGI_APPLICATION = 'nft.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
