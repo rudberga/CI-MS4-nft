e0ae80152680dff1fe644299455287e25ee5e1fc
rudberga <rudberg@pm.me>
2021-04-30 11:50:32 +0000

    ADD - comments app with model and urls

---
+++ comments/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+from pieces.models import Piece
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    piece = models.ForeignKey(Piece, on_delete=models.CASCADE)
+    piece_comment = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
