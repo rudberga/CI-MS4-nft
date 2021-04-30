914025a6e7631b14430c7e087269a6ace72f9c1e
rudberga <rudberg@pm.me>
2021-04-30 21:33:47 +0000

    ADD - deployment to Heroku

--- nft/settings.py
+++ nft/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-0ad341b8oa4%nz*hu5_opdnuiosp+_l)6g)def#+lb(owk889^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rudberga-nft.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -132,12 +132,17 @@ WSGI_APPLICATION = 'nft.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
