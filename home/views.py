181d26867ba3b7242179b8c08f7db15089755ff7
rudberga <rudberg@pm.me>
2021-05-03 18:01:34 +0000

    ADD - login modal

--- home/views.py
+++ home/views.py
@@ -1,10 +1,15 @@
+from allauth.account.forms import LoginForm, SignupForm
 from django.shortcuts import render
 
 
 def index(request):
+    context = {
+        'login_form': LoginForm(),
+        'signup_form': SignupForm(),
+    }
     """ View to return index page """
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
 
 
 def about(request):
